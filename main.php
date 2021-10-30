2946f0f932975539d0633ca6e890e1cfdc34f951
bryanalexisperezb19 <91656436+bryanalexisperezb19@users.noreply.github.com>
2021-10-30 02:17:26 -0600

    Create main.php
    Explicación del código
    2 se declara la variable $num1 y a la vez se le asigna 7
    3 se declara la variable $num2 y a la vez se le asigna 20
    4 se declara la variable $num3 y a la vez se le asigna 3
    6 la variable $1++ se suma a uno
    8 la variable $num1 ahora se le asigna el valor de 8
    10 el valor asignado a $num1 se le suma el valor de 3
    11 La variable $num1 ahora se le asigna el valor de 11
    13 al valor asignado a $num1 se le multiplica el valor de 10
    14 la variable de $num1 ahora se le asigna el valor de 110
    16 al valor asignado de $num1 se divide entre 10
    17 a la variable $num1 ahora se le asigna el valor de 55
    19 a la variable $suma se le asignan el valor a $num2 mas el valor del $num3
    20 a la variable $multiplicacion se le asigna el valor de $num2 por el valor de la variable $num3
    21 a la variable $resto es el sobrante de la operación del valor asignado de la variable número dos al valor asignado del numero tres

---
+++ main.php
@@ -0,0 +1,26 @@
+<?php
+    $num1 = 7;
+    $num2 = 20;
+    $num3 = 3;
+
+    $num1++;
+    echo "Bryan Alexis Perez Bermudez Gpo 5I<br>";
+    echo "El valor de 'num1' ahora es: [".$num1."]<br>";
+
+    $num1 += 3;
+    echo "El valor de 'num1' ahora es: [".$num1."]<br>";
+
+    $num1 *= 10;
+    echo "El valor de 'num1' ahora es: [".$num1."]<br>";
+
+    $num1 /= 2;
+    echo "El valor de 'num1' ahora es: [".$num1."]<br>";
+
+    $suma = $num2 + $num3;
+    $multiplicacion = $num2 * $num3;
+    $resto = $num2 % $num3;
+
+    echo "La suma es: [".$suma."]<br/>";
+    echo "La multiplicación es: [".$multiplicacion."]<br>";
+    echo "El resto es: [".$resto."]<p/>";
+?>
